834a786ee041e2280d26c3dbdc40b91c16e4465e
vinayak <vk6199180@gmail.com>
2026-06-29 22:14:35 +0530

    Initial Playwright, TypeScript and Cucumber setup

---
+++ cucumber.js
@@ -0,0 +1,14 @@
+module.exports = {
+  default: {
+    require: [
+      "features/step-definitions/*.ts",
+      "features/hooks/*.ts"
+    ],
+    requireModule: ["ts-node/register"],
+    format: [
+      "progress",
+      "html:reports/cucumber-report.html"
+    ],
+    publishQuiet: true
+  }
+};
