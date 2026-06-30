5d280c7999bfbe4165d3a932e8bad0e66bac4db2
vinayak <vk6199180@gmail.com>
2026-06-30 10:13:49 +0530

    Screenshots execution proof

--- features/hooks/hooks.ts
+++ features/hooks/hooks.ts
@@ -1,29 +1,53 @@
-import { Before, After, setDefaultTimeout } from "@cucumber/cucumber";
-import { chromium, Browser, BrowserContext, Page } from "@playwright/test";
-
-setDefaultTimeout(60 * 1000); // 60 seconds
+import {
+  Before,
+  After,
+  AfterStep,
+  setDefaultTimeout
+} from "@cucumber/cucumber";
+setDefaultTimeout(60000); // 60 seconds
+import {
+  chromium,
+  Browser,
+  BrowserContext,
+  Page
+} from "@playwright/test";
 
 export let browser: Browser;
 export let context: BrowserContext;
 export let page: Page;
 
+let stepCounter = 0;
+
 Before(async function () {
-    browser = await chromium.launch({
-        headless: false
-    });
 
-    context = await browser.newContext();
-    page = await context.newPage();
+  stepCounter = 0;
 
-    await page.goto(
-        "https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC",
-        {
-            waitUntil: "domcontentloaded",
-            timeout: 60000
-        }
-    );
+  browser = await chromium.launch({
+    headless: false
+  });
+
+  context = await browser.newContext();
+
+  page = await context.newPage();
+
+  await page.goto("https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC");
+});
+
+AfterStep(async function ({ pickle, pickleStep }) {
+
+  stepCounter++;
+
+  const scenarioName = pickle.name.replace(/[<>:"/\\|?*]/g, "_");
+  const stepName = pickleStep.text.replace(/[<>:"/\\|?*]/g, "_");
+
+  await page.screenshot({
+    path: `screenshots/${scenarioName}/${stepCounter}-${stepName}.png`,
+    fullPage: true
+  });
+
 });
 
 After(async function () {
-    await browser.close();
+  await context.close();
+  await browser.close();
 });
